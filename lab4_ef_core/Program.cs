c58d76cc7385cb60c9d295a5400320ba20c1f878
ali_hwang <123@123.com>
2022-12-08 16:51:39 +0800

    lab4 err

--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -3,10 +3,21 @@ using Model;
 var dbpath = new BlogContext();
 Console.WriteLine($"path:{dbpath.DBPath}");
 
-dbpath.Add(new Blog{Uri="www.systex.com.tw"});
-dbpath.Add(new Blog{Uri="www.systex1.com.tw"});
-dbpath.Add(new Blog{Uri="www.systex2.com.tw"});
+//dbpath.Add(new Blog{Uri="www.systex.com.tw"});
+//dbpath.Add(new Blog{Uri="www.systex1.com.tw"});
+//dbpath.Add(new Blog{Uri="www.systex2.com.tw"});
+//dbpath.SaveChanges();
+
+Console.WriteLine("修改一下資料");
+var blog = dbpath.Blogs!.OrderBy(b => b.BlogId).Last();
+blog.Uri = "https://www.cwb.gov.tw";
+blog.Posts.Add(new Post { Title = "urgent", Content = "Type is coming" });
+blog.Posts.Add(new Post { Title = "urgent", Content = "Earthquake last night" });
 dbpath.SaveChanges();
+
+
+
+
 
 var blogs = dbpath.Blogs.OrderBy(b => b.BlogId);
 foreach(var db in blogs)
