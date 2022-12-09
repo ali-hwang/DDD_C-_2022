cf41251a3d4d291638155aa74dbf1d1f6a451904
ali_hwang <123@123.com>
2022-12-09 17:01:34 +0800

    register application service

--- lab6_together/Together.Api/Program.cs
+++ lab6_together/Together.Api/Program.cs
@@ -1,10 +1,12 @@
+using Together.Application;
 using Together.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     builder.Services.AddControllers();
-    builder.Services.AddScoped<IEventService,EventService>();
+    //builder.Services.AddScoped<IEventService,EventService>();
+    builder.Services.AddApplication();
 }
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
