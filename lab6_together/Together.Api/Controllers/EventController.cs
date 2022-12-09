130afe74afe8777228bde2e23a846d2f0ef1f0a9
ali_hwang <123@123.com>
2022-12-09 13:58:47 +0800

    add api 2 event

--- lab6_together/Together.Api/Controllers/EventController.cs
+++ lab6_together/Together.Api/Controllers/EventController.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Together.Api.Controller;
 
-using Microsoft.AspNetCore.Mvc;
 [ApiController]
 [Route("api")]
 public class EventController : ControllerBase
 {
     [Route("add-event")]
     public IActionResult AddEvent()
+    {
+        return Ok();
+    }
+    [Route("query-event")]
+    public IActionResult QueryEvent()
     {
         return Ok();
     }
