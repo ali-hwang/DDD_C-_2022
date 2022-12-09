d7901bdd6e082be447e389d7bde6a592d787d1c3
ali_hwang <123@123.com>
2022-12-09 13:40:12 +0800

    add new event route

--- lab6_together/Together.Api/Controllers/EventController.cs
+++ lab6_together/Together.Api/Controllers/EventController.cs
@@ -3,7 +3,11 @@ namespace Together.Api.Controller;
 using Microsoft.AspNetCore.Mvc;
 [ApiController]
 [Route("api")]
-public class EventController:ControllerBase
+public class EventController : ControllerBase
 {
-
+    [Route("add-event")]
+    public IActionResult AddEvent()
+    {
+        return Ok();
+    }
 }
